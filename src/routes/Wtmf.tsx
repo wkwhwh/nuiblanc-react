ce1b4e188b14af9df713538d143870800e8e0373
Marc David <7226788+wkwhwh@users.noreply.github.com>
2025-04-02 11:08:14 +0200

    fix: remove wtmf online shop links

--- src/routes/Wtmf.tsx
+++ src/routes/Wtmf.tsx
@@ -3,14 +3,6 @@ import ArtworkWtmf from "../components/ArtworkWtmf";
 
 export default function App() {
   const items = [
-    {
-      icon: "spotify" as const,
-      link: "https://open.spotify.com/album/1mmJZ5edBZa7JXgGzk09iI",
-    },
-    {
-      icon: "itunes-note" as const,
-      link: "https://music.apple.com/gb/album/when-the-machine-fails-ep/1682543502",
-    },
     { icon: "youtube" as const, link: "https://youtu.be/ETO0XTtEPHk" },
     {
       icon: "bandcamp" as const,
